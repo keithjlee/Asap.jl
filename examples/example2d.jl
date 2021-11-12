68023e9219fe59a629df4bae60bc5b3dcf64747e
kjl <keithjl@mit.edu>
2021-11-12 11:20:23 -0500

    Start tests; revised 2D frame transformation matrix

--- examples/example2d.jl
+++ examples/example2d.jl
@@ -48,7 +48,7 @@ begin
     elements = [e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13]
 end
 
-# create loads
+# create force vectors [Fx, Fy (, M)]
 if structureType == :truss
     forces = [0.0, -20.0]
 else
@@ -57,6 +57,7 @@ end
 
 begin
     p1 = Load(nodes, nodes[6].position, forces)
+    p1 = Load(nodes, [22.5, 0.], forces) #equivalent
     p2 = Load(nodes, nodes[7].position, forces)
     p3 = Load(nodes, nodes[8].position, forces)
 
@@ -65,6 +66,9 @@ end
 
 # assemble structure
 truss = Structure(nodes, elements, loads)
+
+# mass of structure
+mass = structureMass(truss, 8000) #kg
 
 # analyze
 @time analyze(truss)
@@ -75,54 +79,3 @@ geo = Geometry(truss)
 # display figure (this is quite crude at the moment)
 # scaleToForce scales line thicknesses to axial force values
 fig = structurePlot(geo; scaleToForce = true)
-
-
-# 3 main data structures: Node, Element, Structure
-# Look in types.jl for the data fields and different function methods of creating each type
-
-# defining all nodes: Node([position], type, boundary conditions)
-node1 = Node([72.0, 0.0, 0.0], :truss, :yfixed)
-node2 = Node([0.0, 36.0, 0.0], :truss, :fixed)
-node3 = Node([0.0, 36.0, 72.0], :truss, :fixed)
-node4 = Node([0.0, 0.0, -48.0], :truss, :fixed)
-
-# The following is equivalent: Node([position], [DOFs])
-node1 = Node([72.0, 0.0, 0.0], [true, false, true])
-node2 = Node([0.0, 36.0, 0.0], [false, false, false])
-node3 = Node([0.0, 36.0, 72.0], [false, false, false])
-node4 = Node([0.0, 0.0, -48.0], [false, false, false])
-
-# group nodes together
-nodes = [node1, node2, node3, node4]
-
-# Material properties
-E = 1.2e6
-
-# defining elements
-e1 = Element(nodes, [1,2], E, 0.302)
-e2 = Element(nodes, [1,3], E, 0.729)
-e3 = Element(nodes, [1,4], E, 0.187)
-
-# group elements
-elements = [e1, e2, e3]
-
-# loads
-l1 = Load(nodes, node1.position, [0.0,0.0,-1000.0])
-
-# group loads (in this case just 1)
-loads = [l1]
-
-# assemble structure
-structure = Structure(nodes, elements, loads)
-
-# analyze structure
-analyze(structure)
-
-# relevant information is now stored in the Structure datatype, e.g.
-displacements = structure.U
-
-# the Geometry type stores things like displaced positions for visualization
-geo = Geometry(structure)
-
-# structurePlot is used to plot the Geometry type
-fig = structurePlot(geo)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,43 @@ using Asap
 using Test
 
 @testset "Asap.jl" begin
-    # Write your tests here.
+    # 2D truss test: Example 3.8 from Kassimali "Matrix Analysis of Structures 2e"
+    # in kips, ft
+
+    #nodes
+    n1 = Node([0., 0.] .* 12, :truss, :fixed)
+    n2 = Node([12., 0.] .* 12, :truss, :fixed)
+    n3 = Node([24., 0.] .* 12, :truss, :fixed)
+    n4 = Node([12., 16.] .* 12, :truss, :free)
+
+    nodes = [n1, n2, n3, n4]
+
+    # elements
+    E = 29e3 #ksi
+
+    e1 = Element(nodes, [1,4], E, 8.)
+    e2 = Element(nodes, [2,4], E, 6.)
+    e3 = Element(nodes, [3,4], E, 8.)
+
+    elements = [e1, e2, e3]
+
+    # load
+    l1 = Load(nodes, n4.position, [150., -300.])
+
+    loads = [l1]
+
+    # assemble
+    ex38 = Structure(nodes, elements, loads)
+
+    # Analyze
+    analyze(ex38)
+
+    # value in textbook
+    d_textbook = [0.21552, -0.13995]
+
+    @test d_textbook ≈ round.(n4.disp, digits = 5)
+
+
+    #2D frame test: Example 7.3 from Ferreira "Matlab Codes for Finite Element Analysis"
+    
 end
